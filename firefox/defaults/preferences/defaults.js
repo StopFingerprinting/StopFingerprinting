1928310eab9e639581709db355766be06f91bfb7
Patricio Palladino <patricio.palladino@inria.fr>
2013-04-19 14:49:21 +0200

    Bump version

--- firefox/defaults/preferences/defaults.js
+++ firefox/defaults/preferences/defaults.js
@@ -2,6 +2,6 @@ pref("extensions.stopfingerprinting.browserId", "");
 pref("extensions.stopfingerprinting.submitUrl", "https://stopfingerprinting.irisa.fr/fingerprint/submit");
 pref("extensions.stopfingerprinting.flashFingerprinterUrl", "https://stopfingerprinting.irisa.fr/fingerprint/flash");
 pref("extensions.stopfingerprinting.submitInterval", 3600000); //One hour
-pref("extensions.stopfingerprinting.version", "0.1.10.1");
+pref("extensions.stopfingerprinting.version", "0.1.11");
 pref("extensions.stopfingerprinting.fingerprintsCount", 0);
 pref("extensions.stopfingerprinting.lastFingerprint", "");
